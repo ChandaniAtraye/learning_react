9eda5a689559a2ce5e81702b50d0dd867269ffc1
ChandaniAtraye <138958904+ChandaniAtraye@users.noreply.github.com>
2026-05-21 23:54:24 +0530

    array with examples

--- my-basic-concepts/src/components/Arraye_Example.jsx
+++ my-basic-concepts/src/components/Arraye_Example.jsx
@@ -12,6 +12,30 @@ function FruitListArray() {
     const sortnum=[...num].sort((a,b)=>b-a);
     const reversenum=[...num].reverse();
 
+    const users=[
+        {id:1,name:"kaustubh"},
+        {id:2,name:"John"},
+        {id:3,name:"Kumar"}
+    ];
+    const user =users.find(u=>u.id===1);
+
+    const fruits1 =["Apple","Banana","Mango","orange"];
+    const fruitIndex =fruits1.findIndex((fruit)=>fruit ==="Apple")
+
+     const numbers=[1,2,3,4,5,6];
+     const res = numbers.reduce((sum,num)=>{
+        return sum+num;
+     },0);
+
+     const names1 =["mayuri","pallavi","Bunty","Bittu","Gaurav"];
+     const results1 = names1.filter(name=>name.includes("B"));
+
+     const names2 =["mayuri","pallavi","Bunty","Bittu","Gaurav","Vaishavi"];
+     const checkName = names2.some((item)=>item.startsWith("V"));
+
+     const numbers3 =[10,20,30,40];
+     const result3 = numbers3.every((num)=>num > 5);
+
   return (     
     <>
     <div className="container">
@@ -22,16 +46,19 @@ function FruitListArray() {
             <li key={index}> {item}</li>
         ))}
     </ul>
+    <hr/>
     <h2> Combined Array Example </h2>
     <ul>
         {combinedItems.map((item,index)=>(
             <li key={index}>{item}</li>
         ))}
     </ul>
+    <hr/>
     <h2> Filter Example</h2>
     {result.map((item,index)=>(
         <li key={index}>{item}</li>
     ))}
+    <hr/>
 
     <h2>Sort Number</h2>
     <ul>
@@ -39,6 +66,7 @@ function FruitListArray() {
             <li key={index}>{item}</li>
         ))}
     </ul>
+    <hr/>
 
 
     <h2>Reverse Number</h2>
@@ -47,6 +75,35 @@ function FruitListArray() {
             <li key={index}>{item}</li>
         ))}
     </ul>
+    <hr/>
+
+    <h2> find Example</h2>
+    <div className="className">
+        <h2>{user.name}</h2>
+    </div>
+    <hr/>
+
+    <h2> Find Index Example</h2>
+    <h4> {fruitIndex}</h4>
+    <hr/>
+
+    <h2> Reduce Example </h2>
+    <h3> {res}</h3>
+    <hr/>
+
+    <h2> Includes Example </h2>
+    <h3>{results1}</h3>
+    <hr/>
+
+    <h2>Some Method example</h2>
+    <h3>{checkName ? "V name found": "No name found"}</h3>
+    <hr/>
+
+
+    <h2>Every Example </h2>
+    <h3>Numbers :{numbers3.join(".  ")}</h3>
+    <h3> Result:{result3 ? "All numbers are greater than 5":"Some numbers are smaller"}</h3>
+
    
     </div>
     </>
